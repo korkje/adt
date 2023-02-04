14b040b555508e310f1ba2e8000dd95a5e0e2703
Ola Næss Kaldestad <mail@okal.no>
2023-02-04 22:55:34 +0100

    remove unnecessary submodule exports for now

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,9 @@ import dts from "vite-plugin-dts";
 
 export default defineConfig({
     build: {
-        sourcemap: true,
         lib: {
-            // entry: resolve(__dirname, "src/index.ts"),
-            entry: {
-                "index": resolve(__dirname, "src/index.ts"),
-                "lib/adt": resolve(__dirname, "src/lib/adt.ts"),
-                "lib/if_let": resolve(__dirname, "src/lib/if_let.ts"),
-                "lib/match": resolve(__dirname, "src/lib/match.ts"),
-                "lib/option": resolve(__dirname, "src/lib/option.ts"),
-                "lib/result": resolve(__dirname, "src/lib/result.ts"),
-                "lib/unwrap": resolve(__dirname, "src/lib/unwrap.ts"),
-            },
-            formats: ["es"],
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "adt",
         },
     },
     plugins: [dts()],
